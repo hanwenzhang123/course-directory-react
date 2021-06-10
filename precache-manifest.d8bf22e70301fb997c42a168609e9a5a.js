16ab907e6016ae8a1596b27c5daf5765b1c4d382
hanwenzhang123 <hanwenzhang888@gmail.com>
2021-06-10 12:16:05 -0400

    Updates

---
+++ precache-manifest.d8bf22e70301fb997c42a168609e9a5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f71b794753ff80d492c4",
+    "url": "/course-directory-react/static/css/main.150c3f9b.chunk.css"
+  },
+  {
+    "revision": "f71b794753ff80d492c4",
+    "url": "/course-directory-react/static/js/main.f71b7947.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory-react/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "417e17ad5a512937e292",
+    "url": "/course-directory-react/static/js/runtime~main.417e17ad.js"
+  },
+  {
+    "revision": "a84a249a9bc0aa923f3976032286b65a",
+    "url": "/course-directory-react/index.html"
+  }
+];
